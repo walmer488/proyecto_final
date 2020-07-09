854ca96550d8453de2eb82cd96409fd90b0afcb0
Walmer <walmermartinez168@gmail.com>
2020-07-09 16:47:32 -0300

    CLase con mi nombre dentro de ella estan mis datos

---
+++ src/proyecto_final/Walmer.java
@@ -0,0 +1,11 @@
+package proyecto_final;
+
+public class Walmer {
+    /*
+    Nombre Completo: Walmer Esteban Guido Martínez.
+    Numero de Carnet: 190319
+    Grupo Practico: Sistemas 22 "A"
+    Carrera: Tecnico en Ingenieria en sistemas Informaticos.
+    
+    */
+}
